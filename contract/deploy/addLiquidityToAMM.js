87047a7acb5e92fdcccde3fa9b3ac248d2048104
anilhelvaci <anilhelvaci96@gmail.com>
2022-09-20 08:26:25 +0300

    #15 addLiquidityToAMM.js script added

---
+++ contract/deploy/addLiquidityToAMM.js
@@ -0,0 +1,45 @@
+import dappConstants from '../dappConstants.js';
+import { E } from '@endo/far';
+
+const addLiquidityToAMM = async homeP => {
+  const home = await homeP;
+  /** @type {{zoe: ZoeService}} */
+  const { zoe, wallet, agoricNames } = home;
+
+  const walletBridgerP = E(wallet).getBridge();
+  const ammInstanceP = E(agoricNames).lookup('instance', 'amm');
+
+  console.log('Fetchin ammPublicFacet...');
+  /** @type XYKAMMPublicFacet */
+  const ammPublicFacet = await E(zoe).getPublicFacet(ammInstanceP);
+
+  const addLiquidityConfig = {
+    id: `${Date.now()}`,
+    invitation: E(ammPublicFacet).makeAddLiquidityInvitation(),
+    proposalTemplate: {
+      give: {
+        Secondary: {
+          pursePetname: 'Secondary Purse',
+          value: 2n ** 10n ** 8n, // 2 Secondary
+        },
+        Central: {
+          pursePetname: 'Agoric stable local currency',
+          value: 1n * 10n ** 6n, // 1 IST
+        },
+      },
+      want: {
+        Liquidity: {
+          pursePetname: 'LpToken Purse',
+          value: 100n,
+        }
+      },
+    },
+  };
+
+  console.log('Making an offer to add liquidity to AMM...');
+  await E(walletBridgerP).addOffer(addLiquidityConfig);
+  console.log('Please go to your wallet UI and approve the offer.');
+
+};
+
+export default addLiquidityToAMM;
